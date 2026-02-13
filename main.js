0173b5a01a16c079d24344436b0ed164b7c761e2
Gontee <gontee.j01@gmail.com>
2026-02-13 12:41:45 +0700

    work in progress, fetched pet data

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ async function start() {
   document.querySelector("#temperature-output").textContent = ourTemperature
 }
 start()
+
+async function petsArea() {
+  const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+  const petsData = await petsPromise.json()
+  petsData.forEach(pet => {
+    console.log(pet.name)
+  })
+}
+petsArea()
